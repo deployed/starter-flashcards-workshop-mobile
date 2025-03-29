0dc8690dc18910ff65e0d0a940a6cdb2e0dc089c
JH <joanna.hulek@gmail.com>
2025-03-29 12:21:14 +0100

    routing to challenge.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { useTranslation } from 'react-i18next';
 
 import { BackgroundContainer, Button, Popup, Typography } from '@/components';
+import { useCreateFlashCardSet, useChallenge } from '@/hooks';
+
 import LogoIcon from '../assets/svgs/logo.svg';
-import { useState } from 'react';
-import { useCreateFlashCardSet} from '@/hooks';
+
+// import {useRouter} from "expo-router";
 
 export default function Index() {
   const { t } = useTranslation();
   // const router = useRouter();
   const [popupVisible, setPopupVisible] = useState(false);
-  const { mutate } = useCreateFlashCardSet();
+  const { mutate: createSet } = useCreateFlashCardSet();
+  const { mutate: createChallenge } = useChallenge();
 
   const handleCreateFlashCards = () => {
-      setPopupVisible(true)
-  }
+    setPopupVisible(true);
+  };
 
   const handleSave = (name: string) => {
     if (!name.trim()) {
@@ -23,22 +27,25 @@ export default function Index() {
       return;
     }
 
-    mutate(name);
+    createSet(name);
   };
 
+  const handleChallenge = () => {
+    createChallenge();
+  };
 
   return (
     <BackgroundContainer imagePath={require('../assets/images/home.png')}>
       <View style={styles.innerContainer}>
-      <Popup
-        visible={popupVisible}
-        onClose={() => setPopupVisible(false)}
-        onSave={handleSave}
-      />
+        <Popup visible={popupVisible} onClose={() => setPopupVisible(false)} onSave={handleSave} />
         <LogoIcon />
         <View style={styles.content}>
           <Typography>{t('home.createFlashcards')}</Typography>
           <Button onPress={handleCreateFlashCards}>{t('home.startHere')}</Button>
+        </View>
+        <View style={styles.content}>
+          <Typography>{t('home.challengeYourself')}</Typography>
+          <Button onPress={handleChallenge}>{t('home.testYourself')}</Button>
         </View>
       </View>
     </BackgroundContainer>
@@ -53,8 +60,8 @@ const styles = StyleSheet.create({
     paddingVertical: 100,
   },
   content: {
-    rowGap: 10, 
-    marginBottom: 10, 
+    rowGap: 10,
+    marginBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
