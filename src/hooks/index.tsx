a8768f37d2a742bd16f9ac018c96ed030763c809
pawelk12 <pawel.krempa12@gmail.com>
2025-03-29 11:07:59 +0100

    init

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,5 @@
 export { useCreateFlashCardSet } from './useCreateFlashCardSet';
-export { useDeleteFlashCardSet } from './useDeleteFlashCardSet';
+// export { useDeleteFlashCardSet } from './useDeleteFlashCardSet';
 export { useEditFlashCard } from './useEditFlashCard';
-export { useMarkAsKnown } from './useMarkAsKnown';
-export { useMarkAsUnknown } from './useMarkAsUnknown';
+// export { useMarkAsKnown } from './useMarkAsKnown';
+// export { useMarkAsUnknown } from './useMarkAsUnknown';
