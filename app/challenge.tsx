daf0869ef9bd10d7cc2cb4bf72008b9268e8b834
JH <joanna.hulek@gmail.com>
2025-03-29 15:24:55 +0100

    challenges added

--- app/challenge.tsx
+++ app/challenge.tsx
@@ -1,22 +1,60 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'expo-router';
+import { useTranslation } from 'react-i18next';
 
 import { fetchFlashCardSets } from '@/api/challenges';
 import { queryKeys } from '@/api/queryKyes';
-import { BackgroundContainer, Typography } from '@/components';
-import { useCreateFlashCardSet } from '@/hooks';
+import { BackgroundContainer, Button, Typography } from '@/components';
+import { useQuestions } from '@/hooks';
 
 const Challenge = () => {
-  const { mutate } = useCreateFlashCardSet();
+  const { t } = useTranslation();
+
+  // const {mutate} = useCreateFlashCardSet();
+
   const { data } = useQuery({ queryKey: queryKeys.sets(), queryFn: fetchFlashCardSets });
+  const router = useRouter();
+
+  console.log(data);
 
   return (
     <BackgroundContainer>
-        <Typography size="LARGE" font="REGULAR">
-            Uzupełnij ten widok :)
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.innerContainer}>
+          <RenderSets data={data}></RenderSets>
+
+          <Typography size="LARGE" font="REGULAR">
+            {t('challenge.createNewFlashcards')}
           </Typography>
+          <View style={styles.content}>
+            <Button onPress={router.back}>{t('challenge.back')}</Button>
+          </View>
+        </View>
+      </ScrollView>
     </BackgroundContainer>
+  );
+};
+
+const RenderSets = ({ data = [] }) => {
+  console.log(data);
+
+  const { mutate: createQuestions } = useQuestions();
+
+  const handleQuestions = () => {
+    createQuestions();
+  };
+
+  return (
+    <View>
+      {data.map((cardSet) => (
+        <Typography key={cardSet.id} size="LARGE" font="REGULAR">
+          <Button onPress={handleQuestions}>{cardSet.title}</Button>
+        </Typography>
+      ))}
+    </View>
   );
 };
 
